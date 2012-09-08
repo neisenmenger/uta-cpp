27740ef7b49d01ea967f2319d08cbea8478d56f0
Colin Carroll <colinc@spiceworks.com>
2012-09-08 15:47:31 -0500

    A solution to problem 7.  Would like to rewrite it using a sieve, but not entirely sure the best way to go about that.

---
+++ project_euler/problem7.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+bool isprime(long n){
+  int rt = pow(n,0.5);
+  
+  if (n <= 1||int(n) != n){
+    return false;
+  }
+  
+  if (n==2){
+    return true;
+  }
+  if (n%2==0){
+    return false;
+  }
+  
+  for(long p = 3;p<=rt;p+=2){
+    if (n%p == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+int main(){ // computes the (top) prime
+  int count = 1; // starting at 1, and skipping 2, since it is already prime
+  long top = 10001; 
+  int n = 1;
+  while (count<top){
+    n+=2;
+    if (isprime(n)){
+      count++;
+      }
+  }
+  cout << "Prime number "<<top<< " is " <<n <<".\n";
+  return n;
+}
+
+
