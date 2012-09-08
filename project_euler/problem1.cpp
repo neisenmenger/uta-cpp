0c2a4c9eef22d0ebd6fbe5d9ed5f8ca490296f36
Colin Carroll <colinc@spiceworks.com>
2012-09-08 13:29:11 -0500

    A solution to problem 1

---
+++ project_euler/problem1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int n = 3;
+  int sum = 0;
+  while (n<1000){
+    if (n%3 == 0 || n%5 == 0){
+      sum+=n;
+    }
+    n+=1;
+  }
+  cout << sum << '\n';
+  return 0;
+}
