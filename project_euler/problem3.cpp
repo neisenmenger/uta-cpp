5ca7007b22da5effe649916b2a6d767613958b65
Colin Carroll <colinc@spiceworks.com>
2012-09-08 15:19:04 -0500

    Updated problem 3 to use vectors, and worked on the text output a little bit.

--- project_euler/problem3.cpp
+++ project_euler/problem3.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 
 using namespace std;
 
 int main(){
-  long n = 600851475143;
-  int p = 3;
+  long N = 1241987098632;
+  long n = N;
+  long p = 3;
+  float rt = pow(n,0.5);
+  vector<long> factors;
+  
   while (n%2 == 0) {
-    cout << 2 << '\n';
+    factors.push_back(2);
     n = n/2;
     }
-  while (n>1){
+  while ((n>1) && (p <= rt)){
     while (n%p==0){
-      cout << p << '\n';
+      factors.push_back(p);
       n = n/p;
     }
     p+=2;
   }
+  if (n>1){
+    factors.push_back(n);
+  }
+  cout << "Factors of "<<N<<": \n\t";
+  for (vector<int>::size_type i = 0; i<factors.size()-1;i++){
+    cout << factors[i]<<", ";
+  }
+  cout << "and " << factors[factors.size()-1] << ".\n";
+  
 }
